e5c781446390cfe447395389d59f732d801485cf
Mike Solomon <mike.solomon@coinbase.com>
2025-06-08 13:24:56 -0400

    fix hydration

--- src/app/components/TokenIconFallback.tsx
+++ src/app/components/TokenIconFallback.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 
 export const TokenIconFallback: FC = () => {
   return (
-    <div className="w-4 h-4 rounded-full bg-zinc-700 flex items-center justify-center">
+    <span className="inline-flex w-4 h-4 rounded-full bg-zinc-700 items-center justify-center">
       <span className="text-[10px]">🪙</span>
-    </div>
+    </span>
   );
 };
 
